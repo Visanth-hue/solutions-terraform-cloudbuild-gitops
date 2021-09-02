c19523ea1becd81b1b4d68567b75743bd3368492
Visanth-hue <visanth007@gmail.com>
2021-09-02 17:21:30 +0000

    dd

--- environments/dev/sql.tf
+++ environments/dev/sql.tf
@@ -1,7 +1,13 @@
+data "google_compute_network" "private-network" {
+  name = "test-ingress"
+}
+
+
+
 module "omni-postgres" {
   source                          = "../../modules/sql/"
   project = var.project
   name                = pg-dev-omni
   database_version    = "POSTGRES_13"
   region              = "europe-west1"
-  private_network = "projects/ecstatic-backup-319018/global/networks/test-ingress"
+  private_network = google_compute_network.private_network.id
