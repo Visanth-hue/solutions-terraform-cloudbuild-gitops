44bed7921b64cda92bab107a84e5d281d5dc6ece
Visanth-hue <visanth007@gmail.com>
2021-09-03 08:15:16 +0000

    d1

--- environments/dev/sql.tf
+++ environments/dev/sql.tf
@@ -9,7 +9,7 @@ module "omni-postgres" {
   deletion_protection = false
 
   // Master configurations
-  tier                            = "db-n1-standard-1"
+  tier                            = "db-custom-2-3.75"
   zone                            = "us-central1-c"
   availability_type               = "ZONAL"
   maintenance_window_day          = 7
