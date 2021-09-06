4b57a6e3f97e24d8faa04639bf218008aaa73f3b
visanth007 <visanth007@gmail.com>
2021-09-06 14:11:34 +0000

    create gke

--- environments/dev/versions.tf
+++ environments/dev/versions.tf
@@ -12,7 +12,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 terraform {
-  required_version = "~> 1.0.0"
+  required_version = ">=0.13"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "= 3.76.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+  }
 }
