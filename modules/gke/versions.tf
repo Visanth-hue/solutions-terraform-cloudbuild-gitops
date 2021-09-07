405829ec5707b658881f7321d57a24941efd03bb
visanth007 <visanth007@gmail.com>
2021-09-07 06:48:06 +0000

    test

--- modules/gke/versions.tf
+++ modules/gke/versions.tf
@@ -2,15 +2,12 @@ terraform {
   required_version = ">=0.13"
 
   required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = ">= 3.39.0, <4.0.0"
-    }
+
 
     google = {
       source  = "hashicorp/google"
       version = "> 3.77.0, <4.0.0"
-      configuration_aliases = [ google.alternate ]
+     
     }
 
     kubernetes = {
