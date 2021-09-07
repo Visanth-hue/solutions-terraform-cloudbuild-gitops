ecf90fe45a4118792334766001667b9a446a56b6
visanth007 <visanth007@gmail.com>
2021-09-07 07:29:18 +0000

    test

--- modules/gke/versions.tf
+++ modules/gke/versions.tf
@@ -2,17 +2,16 @@ terraform {
   required_version = ">=0.13"
 
   required_providers {
-
-
     google = {
       source  = "hashicorp/google"
-      version = "> 3.77.0, <4.0.0"
-     
+      version = ">= 3.39.0 <3.77.0"
     }
-
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "~> 2.0"
     }
+  }
+  provider_meta "google" {
+    module_name = "blueprints/terraform/terraform-google-kubernetes-engine:private-cluster/v16.1.0"
   }
 }
